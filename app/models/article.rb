2db4cfacc0f4c5afbb3a533abc8565285b1848aa
Mathilde Louradour <mathilde.louradour@gmail.com>
2019-08-19 18:03:47 +0200

    articles features

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ApplicationRecord
+  belongs_to :user
+  validates :name, presence: true
+  validates :price, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+jean = User.new(email: "jean@gmail.com", password:"errrrrrr" )
+jean.save
+paul = User.new(email: "Paul@gmail.com", password:"rrrrrferge")
+paul.save
+
+article1 = Article.new(name: "valise", price: 3, user_id: 2)
+article1.save
+article2 = Article.new(name: "sac de couchage", price: 4, user_id: 2 )
+article2.save
+article3 = Article.new(name: "duvet", price: 3, user_id: 1)
+article3.save
+article4 = Article.new(name: "valiseXL", price: 5, user_id: 2)
+article4.save
+
