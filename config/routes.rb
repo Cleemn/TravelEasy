4552a37227ac17e1ef4541b531a4d40da961f0a7
Clementine Nocton <clementinenocton@hotmail.fr>
2019-08-20 18:39:15 +0200

    MAJ status + index bookings

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :articles do
-      resources :bookings, only: [:new, :create]
+      resources :bookings, only: [:new, :create, :index]
   end
 
-  resources :bookings, only: [:show]
+  resources :bookings, only: [:show] do
+    get "accept"
+    get "decline"
+  end
 
 end
 
