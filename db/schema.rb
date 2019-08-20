fe704a6cf378bdd97b848f77ae89d7cc0673bb91
Clementine Nocton <clementinenocton@hotmail.fr>
2019-08-20 10:49:05 +0200

    add photo uploader

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,9 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 2019_08_19_135238) do
+
+ActiveRecord::Schema.define(version: 2019_08_20_083411) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_08_19_135238) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
