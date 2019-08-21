34ec41179a6a855dd27274a69cc14982d49fb9a8
Bertrand Lamache <bertrand.lamache@free.fr>
2019-08-21 14:14:12 +0200

    category article-adress index_by_category

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,6 @@ class Article < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
   mount_uploader :photo, PhotoUploader
+  validates :category, inclusion: { in: %w[valise cabine sac-a-dos],
+                                    message: "%{value} is not a valid category" }
 end
