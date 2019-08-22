7c417de5d6eea9bff5cc28ec42fc3820edbbf91d
Clementine Nocton <clementinenocton@hotmail.fr>
2019-08-22 18:24:51 +0200

    new features

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_08_22_135830) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +43,15 @@ ActiveRecord::Schema.define(version: 2019_08_22_135830) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.text "content"
     t.bigint "article_id"
@@ -52,15 +60,6 @@ ActiveRecord::Schema.define(version: 2019_08_22_135830) do
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_reviews_on_article_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
-  end
-
-  create_table "pg_search_documents", force: :cascade do |t|
-    t.text "content"
-    t.string "searchable_type"
-    t.bigint "searchable_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
   create_table "users", force: :cascade do |t|
