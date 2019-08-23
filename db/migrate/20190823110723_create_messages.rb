d184c9409bc7b29ed404aa62bc31622ea7b4b368
Clementine Nocton <clementinenocton@hotmail.fr>
2019-08-23 16:36:56 +0200

    test

--- db/migrate/20190823110723_create_messages.rb
+++ db/migrate/20190823110723_create_messages.rb
@@ -2,8 +2,8 @@ class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
       t.text :content
-      t.references :user
-      t.references :booking
+      t.references :user, foreign_key: true
+      t.references :booking, foreign_key: true
 
       t.timestamps
     end
